15d86499e4b6f3a78259408a5bc7985c86ca1b31
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2024-07-08 23:46:17 +0200

    Deploy

--- src/stores/MessagesStore.js
+++ src/stores/MessagesStore.js
@@ -9,35 +9,35 @@ export const useMessagesStore = defineStore('messages', {
         messageType: 'defaultWelcomeMessage',
         name: 'Default welcome message',
         variables: ['guestFirstName', 'senderName', 'dayOfWeek'],
-        text: `Hi --guestFirstName--, this is --senderName-- from the Haystack Hostel, I hope you are well and thanks again for booking with us. Could you please let us know what time you will be arriving on --dayOfWeek--?\nPlease bear in mind we are at the very top of our building and there are a quite a few steps to reach reception level. See you soon!`
+        text: `Hi --guestFirstName--, this is --senderName-- from the Haystack Hostel. I hope you are well and thanks again for booking with us. Could you please let us know what time you will be arriving on --dayOfWeek--?\nYou are welcome to leave your bags in reception before check in at 3pm. Please keep in mind we are on the top floor of a four storey building so there are quite a few steps to reach reception. See you soon!`
       },
       {
         custom: false,
         messageType: 'withArrivalTime',
         name: 'With arrival time',
         variables: ['guestFirstName', 'senderName', 'dayOfWeek', 'checkinTime'],
-        text: `Hi --guestFirstName--, this is --senderName-- from the Haystack Hostel, I hope you are well and thanks again for booking with us. We look forward to welcoming you on --dayOfWeek-- --checkinTime--.\nPlease bear in mind we are at the very top of our building and there are a quite a few steps to reach reception level. See you soon!`
+        text: `Hi --guestFirstName--, this is --senderName-- from the Haystack Hostel. I hope you are well and thanks again for booking with us. We look forward to welcoming you on --dayOfWeek-- --checkinTime--.\nPlease keep in mind we are on the top floor of a four storey building so there are quite a few steps to reach reception. See you soon!`
       },
       {
         custom: false,
         messageType: 'earlyCheckin',
         name: 'Early Checkin',
         variables: ['guestFirstName', 'senderName', 'dayOfWeek', 'checkinTime'],
-        text: `Hi --guestFirstName--, this is --senderName-- from the Haystack Hostel, I hope you are well and thanks again for booking with us. We look forward to welcoming you on --dayOfWeek-- --checkinTime--.\nOur official check-in time is at 3pm, however you are very welcome to leave your luggage here at reception, then you are free to explore Edinburgh!\nPlease bear in mind we are at the very top of our building and there are a quite a few steps to reach reception level. See you soon!`
+        text: `Hi --guestFirstName--, this is --senderName-- from the Haystack Hostel. I hope you are well and thanks again for booking with us. We look forward to welcoming you on --dayOfWeek-- --checkinTime--.\nOur official check-in time is at 3pm, however you are very welcome to leave your luggage here at reception, then you are free to explore Edinburgh!\nPlease keep in mind we are on the top floor of a four storey building so there are quite a few steps to reach reception. See you soon!`
       },
       {
         custom: false,
         messageType: 'lateCheckin',
         name: 'Late Checkin',
         variables: ['guestFirstName', 'senderName', 'roomNumber'],
-        text: `Hello --guestFirstName--, this is --senderName-- from the Haystack Hostel.\nAs you are checking in at a time when the reception will be closed, I have put your key in an envelope with your name on it, next to the check out box which is located directly on your right as you walk through the front door.\nThe door code to enter the hostel is 3578. Your room number is --roomNumber--. Turn to your left as you walk in, go down the corridor and there you will find signs to your room.\nThere are bathrooms and showers on your floor as well as next to reception if yours are busy in the morning.\nAnd please switch off any lights you switch on.\nThere is no one at reception just now (although staff is on site in case of emergency) until tomorrow morning 8.00am so just pop by to say hi!`
+        text: `Hello --guestFirstName--, this is --senderName-- from the Haystack Hostel.\n As you are checking in tonight after midnight, the reception will be closed but here are the instructions so you can check yourself in. The door code for the both the street door and the hostel door is *2365*. The Haystack hostel is on the third floor. I will put your Room key with your name on it, on the reception shelf which is located in front of you slightly to the left as you walk through the door to the hostel. Your room number is --roomNumber-- and I have allocated Bed --bedNumber-- for you in the dorm, there is a locker for your luggage also number --bedNumber--. To go to the room, --roomLocation1-- as you walk in the Haystack Hostel door and you will see Room --roomNumber-- --roomLocation2--.`
       },
       {
         custom: false,
         messageType: 'emailMessage',
         name: 'Email Message',
         variables: ['guestFirstName', 'senderName', 'dayOfWeek'],
-        text: `Hi --guestFirstName--, this is --senderName--, I hope you are well and thanks again for booking with us.\nCould you please let us know what time you will be arriving --dayOfWeek-- and also if you have a WhatsApp number?\nPlease bear in mind we are at the very top of our building and there are a quite a few steps to reach reception level. See you soon!`
+        text: `Hi --guestFirstName--, this is --senderName-- from the Haystack Hostel. I hope you are well and thanks again for booking with us.\nCould you please let us know what time you will be arriving on --dayOfWeek-- ? Also, could you please provide us with your WhatsApp number if you have one?\nYou are welcome to leave your bags in reception before check in at 3pm.\nPlease keep in mind we are on the top floor of a four storey building so there are quite a few steps to reach reception. See you soon!`
       }
       /*       {
         custom: false,
